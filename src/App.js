68dacc695fc320fa02baa05e1aac4a5e3d0aceed
github username <github email address>
2020-02-28 16:46:37 -0500

    created basic SideBar, SearchResults

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar'
 import Login from './containers/Login'
+import Sidebar from './components/Sidebar'
+import SearchResults from './components/SearchResults'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <div>App</div>
     <Login />
     <SearchBar />
+    <Sidebar />
+    <SearchResults />
     </div>
     );
 }
