8afe7c102860a626b3109f608528e76653e79aca
Amia Defreitas <amia.defreitas0@gmail.com>
2020-03-02 09:53:18 -0500

    put song array state in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import './App.css';
-import HomeContainer from './containers/HomeContainer';
+import HomeContainer from './containers/Home_Container';
 import Login from './containers/Login'
 import Sidebar from './components/Sidebar'
 import SearchResults from './components/SearchResults'
-import SignUpContainer from './containers/SignupContainer'
+// import SignUpContainer from './containers/SignupContainer'
 
-function App() {
+class App extends React.Component {
 
- 
+  state ={
+    songsArray:[]
+
+  }
+
+
+
+componentDidMount(){  
+    fetch('http://localhost:3000/songs')
+    .then(r => r.json())
+    .then(data => this.setState({
+        songsArray:data
+    }))
+
+
+} 
+
+ render(){
+
   return (
     <div>
-    <div className ='App'></div>
-    <SignUpContainer />
-    <Login />
-    <HomeContainer />
-    <Sidebar />
-    <SearchResults />
+      <div className ='App'></div>
+      {/* <SignUpContainer /> */}
+      <Login />
+      <HomeContainer songsArray={this.state.songsArray}/>
+      <Sidebar />
+      <SearchResults />
     </div>
     );
   }
+}
 
  
 export default App;
--- src/containers/Home_Container.js
+++ src/containers/Home_Container.js
@@ -5,29 +5,14 @@ import Recommended from '../components/Recommended'
 import NowPlaying from '../components/NowPlaying'
 
 class HomeContainer extends React.Component {
-    state ={
-        songsArray:[]
-    
 
-
-    }
-
-    componentDidMount(){  
-        fetch('http://localhost:3000/songs')
-        .then(r => r.json())
-        .then(data => this.setState({
-            songsArray:data
-        }))
-
-
-    } 
 
     render() { 
         return ( 
             <div className = 'home-container'>
                 <SearchBar />
                 <Header />
-                <Recommended songsArray ={this.state.songsArray}/>
+                <Recommended songsArray ={this.props.songsArray}/>
                 <NowPlaying />
             </div>
          );
