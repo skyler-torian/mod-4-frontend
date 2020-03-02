1c32df386e0aa37e278b6a4740eb7292fb9ce731
github username <github email address>
2020-03-02 10:25:22 -0500

    set up state for search bar, and handler to process what is typed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
-import HomeContainer from './containers/Home_Container';
+import HomeContainer from './containers/HomeContainer';
 import Login from './containers/Login'
 import Sidebar from './components/Sidebar'
 import SearchResults from './components/SearchResults'
+
 // import SignUpContainer from './containers/SignupContainer'
 
 class App extends React.Component {
 
   state ={
-    songsArray:[]
+    songsArray:[],
+    searchText: ""
 
+  }
+
+  searchHandler = (event) => {
+    console.log(event.target.value)
   }
 
 
@@ -32,7 +38,7 @@ componentDidMount(){
       <div className ='App'></div>
       {/* <SignUpContainer /> */}
       <Login />
-      <HomeContainer songsArray={this.state.songsArray}/>
+      <HomeContainer songsArray={this.state.songsArray} searchHandler={this.searchHandler}/>
       <Sidebar />
       <SearchResults />
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class SearchBar extends React.Component {
     render() {
-        return <div>This is where the SearchBar will go</div>
+        return <input type="text" placeholder="Search..." className="search bar" onChange={this.props.searchHandler}/>
     }
 }
 
